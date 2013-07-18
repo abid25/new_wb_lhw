8fc9fe58edd2df5362f4418b33261e7a8bdaad8d
Awais Ilyas <awais545@gmail.com>
2013-07-18 10:12:36 +0500

    added delayed job initializer

---
+++ config/delayed_job_config.rb
@@ -0,0 +1,9 @@
+#   # config/initializers/delayed_job_config.rb
+#   Delayed::Job.destroy_failed_jobs = false
+#   silence_warnings do
+#     Delayed::Job.const_set("MAX_ATTEMPTS", 3)
+#     Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
+#   end
+
+# config/initializers/delayed_job_config.rb
+Delayed::Worker.destroy_failed_jobs = false
